3a26d3830fe7b347bf5664457ff9037e38a99937
rodriquelali <rodriquelali@gmail.com>
2025-06-17 20:39:15 -0400

    Add wiget: Container, Colum and wihet.

--- flutter_application_1/lib/view/login.dart
+++ flutter_application_1/lib/view/login.dart
@@ -3,83 +3,46 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
-  @override 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Login Page"),
-        backgroundColor: Colors.amber,
-        
-      ),
-      body: titlo(),
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.amber,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            IconButton(
-              icon: Icon(Icons.home),
-              onPressed: () {},
-            ),
-            IconButton(
-              icon: Icon(Icons.settings),
-              onPressed: () {},
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        
-        onPressed: () {
-          print("Floating Action Button Pressed");
-        },
-        child: Icon(Icons.add),
-        backgroundColor: Colors.amber,
-        
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.amber,
-              ),
-              child: Text(
-                'Drawer Header',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                ),
-              ),
-            ),
-            ListTile(
-              leading: Icon(Icons.message),
-              title: Text('Messages'),
-              onTap: () {
-                // Handle message tap
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.account_circle),
-              title: Text('Profile'),
-              onTap: () {
-                // Handle profile tap
-              },
-            ),
-          ],
-        ),
+      appBar: AppBar(title: Text("Login Page"), backgroundColor: Colors.amber),
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        child: Column(children: [
+          Center(child: titlo(),),
+          SizedBox(height: 20),
+          inputText(),
+          SizedBox(height: 20),
+          inputText()
+        ]),
       ),
     );
   }
 
-  Widget titlo(){
-    return Text("Hola tercero Login B");
+  Widget inputText() {
+    return TextField(
+      decoration: InputDecoration(
+        labelText: "Ingrese su nombre",
+        hintText: "Nombre",
+        border: OutlineInputBorder(),
+        suffixIcon: Icon(Icons.person),
+      ),
+    );
+  }
+
+  Widget titlo() {
+    return Text(
+      "Hola tercero Login B",
+      style: TextStyle(
+        fontSize: 30,
+        color: Colors.blue,
+        fontWeight: FontWeight.bold,
+      ),
+    );
   }
 
   Widget titulo2() {
     return Text("Hola segundo Login B");
   }
 }
-
-
