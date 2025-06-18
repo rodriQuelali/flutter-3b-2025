dc57824dcbf7c5a01df93f660d243e57bb20a08a
rodriquelali <rodriquelali@gmail.com>
2025-06-17 20:19:43 -0400

    Convert a Steless.

--- flutter_application_1/lib/view/login.dart
+++ flutter_application_1/lib/view/login.dart
@@ -1,22 +1,84 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatefulWidget {
+class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
-  @override
-  State<LoginPage> createState() => _LoginPageState();
-  
-}
-
-class _LoginPageState extends State<LoginPage> {
-  @override
+  @override 
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Login Page"),
+        backgroundColor: Colors.amber,
+        
       ),
-      body: Text("holaa")
+      body: titlo(),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.amber,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+              icon: Icon(Icons.home),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.settings),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        
+        onPressed: () {
+          print("Floating Action Button Pressed");
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.amber,
+        
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.amber,
+              ),
+              child: Text(
+                'Drawer Header',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.message),
+              title: Text('Messages'),
+              onTap: () {
+                // Handle message tap
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Profile'),
+              onTap: () {
+                // Handle profile tap
+              },
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  Widget titlo(){
+    return Text("Hola tercero Login B");
+  }
+
+  Widget titulo2() {
+    return Text("Hola segundo Login B");
   }
 }
 
